0080c4b1ad998ad8311753cd21ae1b5d3119d694
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-03 10:37:39 -0500

    Add a query and polish guide

--- complete/src/main/java/hello/PersonRepository.java
+++ complete/src/main/java/hello/PersonRepository.java
@@ -1,10 +1,15 @@
 
 package hello;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
+
+	List<Person> findByLastName(@Param("name") String name);
 
 }

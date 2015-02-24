18f85627db6354251f025a59405c054223d9598a
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 14:19:26 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,16 +2,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableMongoRepositories
-@Import(RepositoryRestMvcConfiguration.class)
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {

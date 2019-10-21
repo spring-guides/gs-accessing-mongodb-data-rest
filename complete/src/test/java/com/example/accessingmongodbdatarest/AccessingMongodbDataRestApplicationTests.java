4b6437cc5153418f6c62396bb8e8ee7bd683ca2d
Jay Bryant <jbryant@pivotal.io>
2019-10-21 13:18:58 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2 and upgraded the tests to JUnit 5. I had to upgrade the Gradle wrapper, too.

--- complete/src/test/java/com/example/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
+++ complete/src/test/java/com/example/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
@@ -16,15 +16,13 @@
 
 package com.example.accessingmongodbdatarest;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -33,7 +31,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AccessingMongodbDataRestApplicationTests {
@@ -44,7 +41,7 @@ public class AccessingMongodbDataRestApplicationTests {
 	@Autowired
 	private PersonRepository personRepository;
 
-	@Before
+	@BeforeEach
 	public void deleteAllBeforeTests() throws Exception {
 		personRepository.deleteAll();
 	}
---
+++ initial/src/test/java/com/example/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMongodbDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

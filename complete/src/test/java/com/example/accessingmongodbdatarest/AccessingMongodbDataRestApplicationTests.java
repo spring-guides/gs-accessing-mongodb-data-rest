4a0765b3123711bba656457f7beda5921a578804
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-10 16:00:06 +0100

    Upgrade to Spring Boot 4.0.2

--- complete/src/test/java/com/example/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
+++ complete/src/test/java/com/example/accessingmongodbdatarest/AccessingMongodbDataRestApplicationTests.java
@@ -20,12 +20,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
